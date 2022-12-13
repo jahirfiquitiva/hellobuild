85482dc5c90820575ef3f9572a90a43a6dac0cc4
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-12-13 16:53:54 -0500

    remove stitches logo use

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,46 +1,5 @@
-import StitchesLogo from '@/components/StitchesLogo';
-import { styled } from '~/stitches';
 import { Auth } from '@/components/Auth';
 
-const Box = styled('div', {});
-
-const Text = styled('p', {
-  fontFamily: '$system',
-  color: '$hiContrast',
-});
-
-const Link = styled('a', {
-  fontFamily: '$system',
-  textDecoration: 'none',
-  color: '$purple600',
-});
-
-const Container = styled('div', {
-  marginX: 'auto',
-  paddingX: '$3',
-
-  variants: {
-    size: {
-      1: {
-        maxWidth: '300px',
-      },
-      2: {
-        maxWidth: '585px',
-      },
-      3: {
-        maxWidth: '865px',
-      },
-    },
-  },
-});
-
 export const LoginPage = () => {
-  return (
-    <Box css={{ paddingY: '$6' }}>
-      <Container size={{ '@initial': '1', '@bp1': '2' }}>
-        LOGIN
-        <Auth />
-      </Container>
-    </Box>
-  );
+  return <Auth />;
 };
