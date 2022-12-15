3039163fb71a46cd74488003ce17cf2f74327ca8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-12-15 13:04:28 -0500

    fix toast styles being overwritten

--- src/utils/toast.ts
+++ src/utils/toast.ts
@@ -3,14 +3,4 @@ import type { ToastOptions } from 'react-hot-toast';
 export const toastConfig: ToastOptions = {
   position: 'top-center',
   className: 'toast',
-  style: {
-    paddingLeft: '24px',
-    gap: '.5rem',
-    maxWidth: '364px',
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    '& > div:firstChild': {
-      fontSize: '1.35rem',
-    },
-  },
 };
